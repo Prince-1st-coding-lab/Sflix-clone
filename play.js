fc579347e1006e8ced0afb1ce869cb6f3300f983
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2025-10-15 13:49:33 +0200

    comit 1

--- play.js
+++ play.js
@@ -3,5 +3,6 @@ function player(){
     div.style.display = "block";
     setTimeout(() => {
         div.style.transform = "translateY(0%)";
+        // div.innerHTML = `<iframe width="1058" height="595" src="https://www.youtube.com/embed/2yobIQSv7IY" title="WE ARE SO COOKED!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
     }, 100);
 }
