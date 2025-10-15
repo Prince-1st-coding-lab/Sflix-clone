6d99e4444e2bd942f567e6e9a1d7ab3522390a53
Prince-1st-coding-lab <princeonedaywill@gmail.com>
2025-10-15 07:47:45 -0700

    commit 2

--- play.js
+++ play.js
@@ -1,8 +1,31 @@
-function player(){
-    var div = document.querySelector(".player");
+function player() {
+    const div = document.querySelector(".player");
     div.style.display = "block";
+
+    // Start the slide-in animation
     setTimeout(() => {
         div.style.transform = "translateY(0%)";
-        // div.innerHTML = `<iframe width="1058" height="595" src="https://www.youtube.com/embed/2yobIQSv7IY" title="WE ARE SO COOKED!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+
+        // --- create iframe but don't insert yet ---
+        const iframeHTML = `<iframe width="853" height="480" src="https://www.youtube.com/embed/qpoBjOg5RHU" title="The Fantastic Four: First Steps | Official Trailer | Only in Theatres July 25" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+
+        // Create a temporary element to hold the iframe
+        const temp = document.createElement("div");
+        temp.innerHTML = iframeHTML;
+
+        // Get the iframe node
+        const iframe = temp.querySelector("iframe");
+
+        // Wait for the iframe to fully load before appending via innerHTML
+        iframe.addEventListener("load", () => {
+            div.innerHTML = iframeHTML; // ✅ only append when ready
+        });
+
+        // Trigger the iframe load by adding it to a hidden element
+        document.body.appendChild(temp);
+        temp.style.display = "none";
+
     }, 100);
 }
+const viewportWidth = window.innerWidth;
+console.log("Viewport Width:", viewportWidth, "pixels");
